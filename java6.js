21829ea5454fb2f9c78cf1ab82342403af36d198
Deideis <d.jirgensons0310@gmail.com>
2026-04-24 09:52:15 +0300

    Add files via upload

---
+++ java6.js
@@ -0,0 +1,26 @@
+/* let cipars = prompt("Lūdzu ievadi ciparu:", "");
+
+if (cipars > 0) {
+    for (let i = 1; i = cipars; i++)
+        console.log(i)
+}
+
+else if (cipars = 0) {
+    console.log("0")
+} else {
+    console.log("Nederīgs skaitlis")
+}
+    */
+
+let cipars = prompt("Lūdzu ievadi ciparu:", "");
+
+if (cipars > 0) {
+    for (let i = 1; i <= cipars; i++)
+        console.log(i)
+}
+
+else if (cipars = 0) {
+    console.log("0")
+} else {
+    console.log("Nederīgs skaitlis")
+}
